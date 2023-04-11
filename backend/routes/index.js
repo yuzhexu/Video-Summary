b298abfed698a1adc4027a562226244e5bb92959
Kebao Quan <kquanlorem@outlook.com>
2023-04-11 18:01:04 -0400

    UPDATE

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -13,54 +13,50 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-
-
-
-const generateResponse = async (url) => {
   // const caption = `
   //   It’s been nine years since Laquan McDonald was killed by police in Chicago, shot in the back while walking away. It’s been seven years since Philando Castile was killed by police in the Minneapolis suburbs, shot while his empty hands were raised during a questionable traffic stop. And it’s been four years since Jacob Harris was killed by police in Phoenix, seconds after he emerged from a car, his back turned. You’ve likely heard less about Harris’ death than you have McDonald’s and Castile’s, but Meg O’Connor’s thorough investigation makes clear that you won’t forget it. The gross miscarriages of justice are plentiful: the circumstances of Harris’ killing and the shifting police statements around it; the money and valuables police took from Harris’ father’s home before informing him his son was dead; the fact that Harris’ friends are currently serving decades-long prison sentences for his death, while the officers who pulled the trigger (and unleashed an attack dog on his prone body) walk free. We’ve heard far, far too many names like McDonald’s and Castile’s and Harris’ over the past decade, and nothing makes me think we won’t continue to hear many more. That’s what makes this sort of journalism so necessary — not because it can bring these young men back to life, but because it makes brutally clear how unjust their deaths are, and how broken policing is.
   // `;
-  var caption;
 
+const generateResponse = async (url) => {
   var options = {
     mode: 'text',
     args: [url]
   };
 
-  PythonShell.run(PythonPath, options, function (err, results) {
-    if (err) 
-    {
-        console.log(err.message);
-        next(err);
-        throw err;
+  try {
+    const result = await PythonShell.run(PythonPath, options);
+    var result_ = JSON.parse(result);
+    var caption = "";
+    for (let i = 0; i < result_.length; i++) {
+      caption += result_[i].text;
+      caption += '\n'
     }
+    console.log("caption: " + caption);
+    var sub_caption;
+    if (caption.length > 2000)
+    {
+      sub_caption = caption.substring(0,1000);
+    }
+    console.log(sub_caption);
+    caption = "abcd";
 
-    caption = JSON.parse(results[0]);
-    caption = "abcd"
-    console.log("caption:1 " + caption)
-    console.log("caption: ")
-  })
 
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(sub_caption),
+      temperature: 0.6,
+      max_tokens: 3500,
+    });
 
-  
-  console.log("caption: " + caption)
-  // (async ()=>{
-  //   const sub = await getSubtitles(url, { timingString: true });
-  //   console.log(sub)
-  // })()
+    return completion;
+  } catch (err) {
+    console.error('Error:', err);
+  }
+};
 
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: generatePrompt(caption),
-    temperature: 0.6,
-    max_tokens: 3000,
-  });
-  
-  return completion;
-}
 
 function generatePrompt(caption) {
-  return `Summarize the following text:
+  return `You are a video summarizer. Summarize the following subtitle from the video:
   ${caption}
   `
 }
